0a9309e3e6784b281d0e63f2c5ac60a080290851
Shegenerates <shegenerates@dancefloordefi.com>
2021-06-06 08:40:14 -0700

    erc721 implementation

---
+++ contracts/Misfit.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract Mifit is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    constructor() ERC721("Misfit", "MFU") {}
+
+    function mint(address player, string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        _tokenIds.increment();
+        require(_tokenIds <= 10000); //10000 item cap
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(player, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        return newItemId;
+    }
+}
