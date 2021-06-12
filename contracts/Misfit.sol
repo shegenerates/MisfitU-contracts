d053de0aa4f1c5040abb5f43a9b96d501c3ee6bf
Shegen <84243160+shegenerates@users.noreply.github.com>
2021-06-12 10:57:08 -0400

    Update Misfit.sol

--- contracts/Misfit.sol
+++ contracts/Misfit.sol
@@ -4,7 +4,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Mifit is ERC721URIStorage {
+contract Misfit is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
