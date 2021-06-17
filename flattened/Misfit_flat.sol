4953c3d2e3accbd189b700136484cc83f8c937ae
shegenerates <shegenerates@ethhole.com>
2021-06-16 22:01:49 -0400

    update flattened with reserves and fees

--- flattened/Misfit_flat.sol
+++ flattened/Misfit_flat.sol
@@ -1005,12 +1005,19 @@ pragma solidity ^0.8.0;
 
 
 
-contract Mifit is ERC721URIStorage {
+contract Misfit is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
     address owner;
     uint fee;
+
+    uint reserved;
+
+    event Minted(address to, uint id, string uri);
+
+    event PriceUpdated(uint newPrice);
+    event OwnerUpdated(address newOwner);
 
     constructor(uint _fee) ERC721("Misfit", "MFU") {
       owner = msg.sender;
@@ -1030,16 +1037,61 @@ contract Mifit is ERC721URIStorage {
         _mint(player, newItemId);
         _setTokenURI(newItemId, tokenURI);
 
+        Minted(player, newItemId, tokenURI);
+
+        return newItemId;
+    }
+
+    function getOwnerResrveMinted() public view returns(uint){
+        return reserved;
+    }
+
+    function mintOwner(address player, string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        require(msg.sender == owner);
+        require(reserved < 100); //owner can mint up to 100 for free. this can be handed over to a DAO too.
+
+        require(_tokenIds.current() < 10000); //10000 item cap
+
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
+        _mint(player, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        Minted(player, newItemId, tokenURI);
+
+        reserved = reserved + 1;
+
         return newItemId;
     }
 
     function updateOwner(address newOwner) public{
       require(msg.sender == owner);
       owner = newOwner;
+
+      OwnerUpdated(newOwner);
     }
 
     function updateFee(uint newFee) public{
       require(msg.sender == owner);
       fee = newFee;
+
+      PriceUpdated(newFee);
+    }
+
+    function getFee() public view returns (uint) {
+      return fee;
+    }
+
+    function getOwner() public view returns (address) {
+      return owner;
+    }
+
+    function cashOut() public{
+        require(msg.sender == owner);
+        msg.sender.transfer(address(this).balance);
     }
 }
