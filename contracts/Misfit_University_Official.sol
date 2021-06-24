87e7ad8a372b9a23982bd273a979e709ff3c4d36
shegenerates <shegenerates@ethhole.com>
2021-06-24 09:40:41 -0400

    update name

--- contracts/Misfit_University_Official.sol
+++ contracts/Misfit_University_Official.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Misfit_University is ERC721URIStorage, Ownable {
+contract Misfit_University_Official is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
